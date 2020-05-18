e82feac0da2eafdfdd1bd33e1e50422c16b0ac5f
Ethan Tsang <1984917185@qq.com>
2020-05-18 20:18:29 +0800

    04修改

--- game/set_3/start_menu/index.js
+++ game/set_3/start_menu/index.js
@@ -39,11 +39,24 @@ var death_role = JSON.parse(sessionStorage.getItem('death_role'))
 console.log(death_role);
 
 
-var option__kill = document.getElementsByClassName('content_option__kill')
-var option__triangle = document.getElementsByClassName('content_option__triangle')
+
+var option_kill = document.getElementsByClassName('content_option__kill')
+var option_triangle = document.getElementsByClassName('content_option__triangle')
+
+var option_ghost = document.getElementById('ghost');
+var newelem = document.getElementById('newelem');
+var day_sun = document.getElementsByClassName('content_day__sun');
+
 
 if(death_role !== null){
-     console.log('z')
-     option__kill[0].style.backgroundColor = '#8dae9b'
-     option__triangle[0].style.borderRight = '1.5rem solid #8dae9b'
+     
+     option_kill[0].style.backgroundColor = '#8dae9b'
+     option_triangle[0].style.borderRight = '1.5rem solid #8dae9b'
+     var newelem = document.createElement('div')
+         newelem.setAttribute('class','newelem')
+     option_ghost.parentNode.insertBefore(newelem,option_ghost)
+     newelem.innerHTML = death_role[0].num + '被玩家杀死，真实身份是'+ death_role[0].name ;
+
+     option_kill[0].style.marginBottom = '1rem'
+     day_sun[0].style.top ='6.7rem'
 }
